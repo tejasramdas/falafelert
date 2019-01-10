fe0d070084fdcd579df553430854206f1d158f55
Tejas Ramdas <tejas.ramdas@gmail.com>
2019-01-09 22:46:36 -0800

    cleaned

--- hello/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Reminder(models.Model):
-    phone_number = models.CharField(max_length=30)
-    food=models.CharField(max_length=12)
